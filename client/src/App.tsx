631fa16418b2fe32fc162fffaa0fa4c74acb7e03
mattferia <matt.feria@clsu2.edu.ph>
2025-10-18 20:43:43 +0800

    me 1

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -64,11 +64,19 @@ function Router() {
 
   return (
     <Switch>
+      {/* Public routes - always accessible */}
+      <Route path="/reset-password">
+        {() => (
+          <Suspense fallback={<LoadingFallback />}>
+            <ResetPassword />
+          </Suspense>
+        )}
+      </Route>
+
       {!isAuthenticated ? (
         <Suspense fallback={<LoadingFallback />}>
           <Route path="/" component={AuthPage} />
           <Route path="/forgot-password" component={ForgotPassword} />
-          <Route path="/reset-password" component={ResetPassword} />
           <Route component={AuthPage} />
         </Suspense>
       ) : (
